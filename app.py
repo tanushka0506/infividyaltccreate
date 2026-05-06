7b3b1dc5effd23423b302282cba7f108fdd18f48
tanushka0506 <tanushka.bhatnagar0506@gmail.com>
2026-05-07 00:56:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,10 +18,40 @@ def log():
     collection.insert_one(data)
     print("Saved:", data)
     return jsonify({"ok": True})
-
 @app.route('/data', methods=['GET'])
 def get_data():
     logs = list(collection.find({}, {"_id": 0}).sort("_id", -1).limit(20))
+    return jsonify(logs)
+
+@app.route('/active', methods=['GET'])
+def active_users():
+
+    logs = list(
+        collection.find({}, {"_id": 0})
+        .sort("timestamp", -1)
+        .limit(100)
+    )
+
+    users = {}
+
+    for log in logs:
+        user = log.get("user")
+
+        if user not in users:
+            users[user] = log
+
+    return jsonify(list(users.values()))
+    
+@app.route('/history/<date>', methods=['GET'])
+def history(date):
+
+    logs = list(
+        collection.find(
+            {"date": date},
+            {"_id": 0}
+        ).sort("timestamp", -1)
+    )
+
     return jsonify(logs)
 
 @app.route('/')
