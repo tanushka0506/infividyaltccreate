e3da7f31969084b6b6d4c648699c70cc348f7f1b
tanushka0506 <tanushka.bhatnagar0506@gmail.com>
2026-05-18 17:30:43 +0530

    Add login and logout functionality for admins

--- app.py
+++ app.py
@@ -9,18 +9,31 @@ client = MongoClient(os.environ.get("MONGO_URI"), serverSelectionTimeoutMS=5000)
 db = client["tracker"]
 employees = db["employees"]
 collection = db["logs"]
-admin=db["admin"]
+admins = db["admins"]
 
 @app.route('/log', methods=['POST'])
 def log():
+
     data = request.json
+
     collection.insert_one(data)
+
     print("Saved:", data)
+
     return jsonify({"ok": True})
+
+
 @app.route('/data', methods=['GET'])
 def get_data():
-    logs = list(collection.find({}, {"_id": 0}).sort("_id", -1).limit(20))
+
+    logs = list(
+        collection.find({}, {"_id": 0})
+        .sort("_id", -1)
+        .limit(20)
+    )
+
     return jsonify(logs)
+
 
 @app.route('/active', methods=['GET'])
 def active_users():
@@ -34,13 +47,15 @@ def active_users():
     users = {}
 
     for log in logs:
+
         user = log.get("user")
 
         if user not in users:
             users[user] = log
 
     return jsonify(list(users.values()))
-    
+
+
 @app.route('/history/<date>', methods=['GET'])
 def history(date):
 
@@ -50,12 +65,17 @@ def history(date):
             {"_id": 0}
         ).sort("timestamp", -1)
     )
+
     return jsonify(logs)
-    
+
+
 @app.route('/users', methods=['GET'])
 def users():
+
     all_users = collection.distinct("user")
+
     return jsonify(all_users)
+
 
 @app.route('/validate-user', methods=['POST'])
 def validate_user():
@@ -67,6 +87,7 @@ def validate_user():
     })
 
     if not employee:
+
         return jsonify({
             "ok": False,
             "message": "Invalid ID"
@@ -76,13 +97,55 @@ def validate_user():
         "ok": True,
         "name": employee["name"]
     })
+
+
 @app.route('/login')
 def login_page():
+
     return open('login.html').read()
-    
+
+
+@app.route('/login', methods=['POST'])
+def login():
+
+    username = request.form.get('username')
+
+    password = request.form.get('password')
+
+    admin = admins.find_one({
+
+        "username": username,
+
+        "password": password
+    })
+
+    if admin:
+
+        session['admin'] = username
+
+        return redirect('/')
+
+    return "Invalid Credentials"
+
+
 @app.route('/')
 def home():
+
+    if 'admin' not in session:
+
+        return redirect('/login')
+
     return open("dashboard.html").read()
 
+
+@app.route('/logout')
+def logout():
+
+    session.pop('admin', None)
+
+    return redirect('/login')
+
+
 if __name__ == "__main__":
+
     app.run()
